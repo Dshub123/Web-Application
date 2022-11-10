a4ec3cbe06f52dab0ae740feea90f376157c28a7
unknown <shubhamdiddi@gmail.com>
2022-11-10 22:57:06 +0530

    webapp

---
+++ webapp/server.js
@@ -0,0 +1,76 @@
+var  express=require('express');
+var app=express();
+//var mysql = require('mysql');
+//middleware setting
+
+app.use(express.static('public'));
+const jsonParser = express.json();
+app.use(jsonParser);
+app.use(express.urlencoded({ extended: true }));
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'shubham',
+//     password: 'shubham',
+//     database: 'db1'
+//   })
+
+
+//map incomming request with callback function
+//Rrequest routing logic
+//HTTP Handlers
+//HTTP Requests are mapped with HTTP handlers
+app.get("/", (req, res)=>{res.sendFile('index.html')});
+app.get("/hello",(req, res)=>{res.send("Welcome");});
+app.get("/aboutus",(req,res)=>{res.send("Transflower Learning Pvt. Ltd.")});
+app.get("/contactus",(req,res)=>{res.send("ravi.tambade@transflower.in")});
+app.get("/products",(req, res)=>{
+
+var flowers=[{"id":12,"title":"Gerbera", "description":"Wedding Flower","unitprice":5,
+              "quatity":5000},
+              {"id":13,"title":"Rose", "description":"valentine Flower","unitprice":15,
+              "quatity":5000},
+              {"id":14,"title":"Lotus", "description":"Worship Flower","unitprice":22,
+              "quatity":5000},
+              {"id":15,"title":"Jasmine", "description":"Smelling Flower","unitprice":5,
+              "quatity":5000},
+              {"id":16,"title":"Tulip", "description":"Wedding Flower","unitprice":5,
+              "quatity":5000},
+              {"id":17,"title":"Orchid", "description":"Wedding Flower","unitprice":5,
+              "quatity":5000}
+            ]
+    res.send(flowers);
+});
+
+app.post("/login",(req, res)=>{
+    console.log(req.body);
+    console.log("data is posted from client");
+    let user=req.body;
+    let status="";
+    if(user.email==="raj.patil@gmail.com" && user.password==="seed"){
+        status="valid user";
+    }
+    else{
+        status="invalid user";
+    }
+    res.send(status);
+})
+
+app.post("/addtocart",(req, res)=>{
+
+})
+
+// connection.connect() 
+  
+//   connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+//     if (err) throw err
+  
+//     console.log('The solution is: ', rows[0].solution)
+//   })
+  
+//   connection.end()
+
+
+app.listen(8888);
+console.log("server is listening on port 8888");
+
